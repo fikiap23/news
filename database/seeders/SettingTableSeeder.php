a02da81a4fac7cb323c37e7e22f520c31de113c7
fikiaprian23 <fikiaprian23@gmail.com>
2024-10-01 22:37:44 +0700

    fix seeder

--- database/seeders/SettingTableSeeder.php
+++ database/seeders/SettingTableSeeder.php
@@ -14,34 +14,34 @@ class SettingTableSeeder extends Seeder
      */
     public function run()
     {
-        $logoUrl = '/assets/image/infyom-logo.png';
-        $favicon = '/assets/image/favicon-infyom.png';
+        $logoUrl = '/assets/image/logo_dpmtsp.png';
+        $favicon = '/assets/image/logo_dpmtsp.png';
 
         Setting::create(['key' => 'application_name', 'value' => 'PTSP']);
-        Setting::create(['key' => 'contact_no', 'value' => '+91 70963 36561']);
-        Setting::create(['key' => 'email', 'value' => 'test@email.com']);
-        Setting::create(['key' => 'copy_right_text', 'value' => 'All Rights Reserved ©2022']);
-        Setting::create(['key' => 'site_key', 'value' => ' ']);
-        Setting::create(['key' => 'secret_key', 'value' => ' ']);
+        Setting::create(['key' => 'contact_no', 'value' => '0821-1617-1515']);
+        Setting::create(['key' => 'email', 'value' => 'dpmptsp@sumedangkab.go.id']);
+        Setting::create(['key' => 'copy_right_text', 'value' => 'Copyrights © 2018 - 2024 DPMPTSP Kabupaten Sumedang']);
+        Setting::create(['key' => 'site_key', 'value' => '1234567890ABCDEF']); // Example site key
+        Setting::create(['key' => 'secret_key', 'value' => 'FEDCBA0987654321']); // Example secret key
         Setting::create(['key' => 'show_captcha', 'value' => 0]);
-        Setting::create(['key' => 'facebook_url', 'value' => 'https://www.facebook.com/infyom/']);
-        Setting::create(['key' => 'twitter_url', 'value' => 'https://twitter.com/infyom?lang=en']);
-        Setting::create(['key' => 'instagram_url', 'value' => 'https://www.instagram.com/?hl=en']);
-        Setting::create(['key' => 'pinterest_url', 'value' => 'https://www.pinterest.com/']);
-        Setting::create(['key' => 'linkedin_url', 'value' => 'https://www.linkedin.com/organization-guest/company/infyom-technologies?challengeId=AQFgQaMxwSxCdAAAAXOA_wosiB2vYdQEoITs6w676AzV8cu8OzhnWEBNUQ7LCG4vds5-A12UIQk1M4aWfKmn6iM58OFJbpoRiA&submissionId=0088318b-13b3-2416-9933-b463017e531e']);
-        Setting::create(['key' => 'vk_url', 'value' => 'https://vk.com/?lang=en']);
+        Setting::create(['key' => 'facebook_url', 'value' => 'https://www.facebook.com']);
+        Setting::create(['key' => 'twitter_url', 'value' => 'https://twitter.com']);
+        Setting::create(['key' => 'instagram_url', 'value' => 'https://www.instagram.com']);
+        Setting::create(['key' => 'pinterest_url', 'value' => 'https://www.pinterest.com']);
+        Setting::create(['key' => 'linkedin_url', 'value' => 'https://www.linkedin.com']);
+        Setting::create(['key' => 'vk_url', 'value' => 'https://vk.com']);
         Setting::create(['key' => 'telegram_url', 'value' => 'https://www.telegram.org/k/']);
         Setting::create(['key' => 'youtube_url', 'value' => 'https://www.youtube.com/']);
         Setting::create(['key' => 'show_cookie', 'value' => 1]);
-        Setting::create(['key' => 'cookie_warning', 'value' => 'Your experience on this site will be improved by allowing cookies.']);
+        Setting::create(['key' => 'cookie_warning', 'value' => 'Pengalaman Anda di situs ini akan ditingkatkan dengan mengizinkan cookie.']);
         Setting::create(['key' => 'logo', 'value' => $logoUrl]);
         Setting::create(['key' => 'favicon', 'value' => $favicon]);
-        Setting::create(['key' => 'contact_address', 'value' => 'C-303, Atlanta Shopping Mall,Nr.Sudama Chowk, Mota Varachha,Surat-394101, Gujarat, India.']);
-        Setting::create(['key' => 'about_text', 'value' => "Leading Web & Mobile Development Company with proven expertise, India's Leading Laravel Open-Source contributor with over 3 million+ Downloads."]);
-        Setting::create(['key' => 'terms&conditions', 'value' => '']);
-        Setting::create(['key' => 'privacy', 'value' => '']);
-        Setting::create(['key' => 'support', 'value' => '']);
+        Setting::create(['key' => 'contact_address', 'value' => 'Jalan Prabu Geusan Ulun No. 36 Sumedang']);
+        Setting::create(['key' => 'about_text', 'value' => "Website DPMPTSP. Portal Dinas Penanaman Modal dan Pelayanan Terpadu Satu Pintu Kabupaten Sumedang."]);
+        Setting::create(['key' => 'terms&conditions', 'value' => 'Syarat dan ketentuan layanan diatur sesuai dengan peraturan yang berlaku.']); // Example terms & conditions
+        Setting::create(['key' => 'privacy', 'value' => 'Kami menghormati privasi Anda dan berkomitmen untuk melindungi informasi pribadi Anda.']); // Example privacy policy
+        Setting::create(['key' => 'support', 'value' => 'Hubungi kami melalui email atau nomor kontak jika membutuhkan bantuan.']); // Example support info
         Setting::create(['key' => 'comment_approved', 'value' => '1']);
-        Setting::create(['key' => 'front_language', 'value' => '1']);
+        Setting::create(['key' => 'front_language', 'value' => '1']); // Example language setting
     }
 }
