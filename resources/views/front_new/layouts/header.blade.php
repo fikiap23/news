a07fc23a7fc6e7172752341588ce6ee52fff9e88
fikiaprian23 <fikiaprian23@gmail.com>
2024-11-01 09:41:23 +0700

    loker

--- resources/views/front_new/layouts/header.blade.php
+++ resources/views/front_new/layouts/header.blade.php
@@ -248,6 +248,7 @@
                                 <li><a class="fs-14 fw-6" href="/visi-misi">Tracking Izin</a></li>
                                 <li><a class="fs-14 fw-6" href="/page2">Validasi SK</a></li>
                                 <li><a class="fs-14 fw-6" href="/page2">Potensi dan Investasi</a></li>
+                                <li><a class="fs-14 fw-6" href="/page2">Lowongan Pekerjaan</a></li>
                             </ul>
                         </li>
                         <li class="border-gray border-end"
