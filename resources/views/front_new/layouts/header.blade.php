bf65178c25194adfe9070d90d26ea0fb656a9050
fikiaprian23 <fikiaprian23@gmail.com>
2024-11-01 09:52:03 +0700

    add izin

--- resources/views/front_new/layouts/header.blade.php
+++ resources/views/front_new/layouts/header.blade.php
@@ -245,6 +245,9 @@
                             </a>
                             <ul class="dropdown-nav ps-0">
                                 <li><a class="fs-14 fw-6" href="/service-standard">Standar Pelayanan</a></li>
+                                <li><a class="fs-14 fw-6" href="/service-standard">Persyaratan Izin</a></li>
+                                <li><a class="fs-14 fw-6" href="/service-standard">Grafik investasi</a></li>
+                                <li><a class="fs-14 fw-6" href="/service-standard">Statistik harian perizinan</a></li>
                                 <li><a class="fs-14 fw-6" href="/visi-misi">Tracking Izin</a></li>
                                 <li><a class="fs-14 fw-6" href="/page2">Validasi SK</a></li>
                                 <li><a class="fs-14 fw-6" href="/page2">Potensi dan Investasi</a></li>
