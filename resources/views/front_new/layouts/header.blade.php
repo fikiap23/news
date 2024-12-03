95fef33fc01f040384cecdca47a61b425682973b
fikiaprian23 <fikiaprian23@gmail.com>
2024-12-03 08:47:07 +0700

    Revert "resize slider"
    This reverts commit 9c67335705878770b1b348e93171f82823b9dddb.

--- resources/views/front_new/layouts/header.blade.php
+++ resources/views/front_new/layouts/header.blade.php
@@ -227,10 +227,12 @@
                                     Dinas</span>
                             </a>
                             <ul class="dropdown-nav ps-0">
+                                <li><a class="fs-14 fw-6" href="/page1">Sejarah</a></li>
                                 <li><a class="fs-14 fw-6" href="/visi-misi">Visi & Misi</a></li>
                                 <li><a class="fs-14 fw-6" href="/organizational-structure">Struktur Organisasi</a>
                                 </li>
                                 <li><a class="fs-14 fw-6" href="/page2">Tugas Pokok dan Fungsi</a></li>
+                                <li><a class="fs-14 fw-6" href="/page2">Daftar Pegawai</a></li>
                                 <li><a class="fs-14 fw-6" href="/page2">Prestasi</a></li>
                             </ul>
                         </li>
